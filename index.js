2622ad7bbd0bb5354bc794c57174a149fb2360ef
Jon Crowell <jcrowell@hksinc.com>
2022-02-16 17:01:59 -0600

    refactor: 💡 make sure we're printing the correct error message

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const { opendir } = require('fs/promises')
 
-const lookForMe = 'E://temp'
+const lookForMe = 'E://tempx'
 
 async function CheckForFolder(folderToCheck) {
   try {
     const dir = await opendir(folderToCheck)
     console.log('Yay! The folder exists. Continue as planned.')
   } catch (err) {
-    console.error(`Error! ${err.path} does not exist!`)
+    console.error(
+      err.errno === -4058 ? `Error! ${err.path} does not exist!` : err,
+    )
   }
 }
 
